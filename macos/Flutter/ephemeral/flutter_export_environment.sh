2c2a84e3309a85b73d1751d00e6bb877415d0e39
flex4660 <147966515+flex4660@users.noreply.github.com>
2025-03-11 19:50:56 +1100

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/feliks.ananda03gmail.com/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/feliks.ananda03gmail.com/AndroidStudioProjects/visualize_infinance"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
