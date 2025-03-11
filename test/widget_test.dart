438d1845f6857e73570ef902ac2ed5113e3bdc02
flex4660 <147966515+flex4660@users.noreply.github.com>
2025-03-11 19:51:53 +1100

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:finney/main.dart';
+import 'package:visualize_infinance/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
