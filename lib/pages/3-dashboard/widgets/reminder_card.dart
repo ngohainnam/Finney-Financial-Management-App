7d3de099faba8a8cce3a89fb2af897fcbeb3d751
ngohainnam <ngohainnam@gmail.com>
2025-05-07 16:09:43 +1000

    fix minor issues

--- lib/pages/3-dashboard/widgets/reminder_card.dart
+++ lib/pages/3-dashboard/widgets/reminder_card.dart
@@ -21,7 +21,7 @@ class ReminderCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
+              color: Colors.grey.withValues(alpha: 0.2),
               blurRadius: 6,
               offset: const Offset(0, 3),
             ),
