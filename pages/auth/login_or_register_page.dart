3e64d0bb7e16354ca363ce7814a89703460f4d26
Feliks <147966515+flex4660@users.noreply.github.com>
2025-05-16 16:36:13 +1000

    New Description On Click in Insights

---
+++ pages/auth/login_or_register_page.dart
@@ -0,0 +1,35 @@
+import 'package:finney/pages/auth/presentation/login_page.dart';
+import 'package:finney/pages/auth/presentation/register_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  //init show login page
+  bool showLoginPage = true;
+
+  //toggle between login and register page
+  void togglePages() {
+    setState(() {
+    showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePages,
+      );
+    } else {
+      return RegisterPage(
+        onTap: togglePages,
+      );
+    }
+  }
+}
